76465202fabad6de29863067b90951cb87ed7e9c
Juan <jtruffa@gmail.com>
2015-06-18 22:43:06 -0300

    Fist delivery

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,35 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    temp <- NULL
+    
+    set <-function(y) {
+        x <<- y
+        temp <<- NULL
+    }
+    
+    get <-function() x
+    
+    guardainversa <- function(matriz) {
+        temp <<- matriz
+    }
+    inversa <- function() temp
+    
+    list(set = set, get = get, guardainversa = guardainveresa, inversa = inversa)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        calcinversa <-x$inversa()
+        if(!is.null(calcinversa)) {
+            message("Getting cached data")
+            return(calcinversa)
+        }
+        
+        valor <- x$get()
+        calcinversa <- solve(valor)
+        x$guardainversa(calcinversa)
+        calcinversa
 }
